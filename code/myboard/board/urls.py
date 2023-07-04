4d8a4b7a603e3e42fb8ea874aa3d753fea70d21e
wwwalohacampus <h850415@naver.com>
2023-07-04 13:51:55 +0900

    ❤

---
+++ code/myboard/board/urls.py
@@ -0,0 +1,14 @@
+
+
+from django.urls import path
+from . import views
+
+app_name = 'board'
+urlpatterns = [
+    # 게시글
+    # '/board/'
+    path('', views.BoardList.as_view(), name='board_list'),
+    
+    # '/board/10'
+    path('/<int:pk>', views.BoardDetail.as_view(), name='board_detail'),
+]
---
+++ code/myboard/board/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
+
+# Create your views here.
+class BoardList(ListView):
+    model = Board
+    
+    
+class BoardDetail(DetailView):
+    model = Board
+    
+    
